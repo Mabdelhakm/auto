a33e1217be6dc20ab8a2a21424bd05b5eeb600f5
loll <notEmail@tt.com>
2024-01-24 22:49:36 +0200

    gfdkj

--- src/test/java/utils/ReadExcel.java
+++ src/test/java/utils/ReadExcel.java
@@ -14,6 +14,7 @@ public class ReadExcel {
 	XSSFSheet sheet;
 	XSSFRow row;
 	public Object[][] read_excel() {
+		//define xlsx path and read the workbook
 		f = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator
 				+ "resources" + File.separator + "file.xlsx");
 		try {
@@ -23,13 +24,20 @@ public class ReadExcel {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		//define the required sheet
 		sheet = wb.getSheet("sheet1");
+		//initialize varibles with number of rows -1 [the header] and the number of rows
 		int num_of_rows = sheet.getPhysicalNumberOfRows();
 		int num_of_cols = sheet.getRow(0).getLastCellNum();
+		//declare the object[][]
 		String[][] obj = new String[num_of_rows - 1][num_of_cols];
+		//loop over all rows next to header
 		for (int i = 1; i < num_of_rows; i++) {
+			//loop over all columns
 			for (int k = 0; k < num_of_cols; k++) {
+				//declare current row 
 				row = sheet.getRow(i);
+				//if the row is null add "" to the object & convert all types into string
 				if (row != null && row.getCell(k) != null) {
 					if (row.getCell(k).getCellType() == CellType.NUMERIC) {
 						obj[i - 1][k] = String.valueOf(row.getCell(k));
