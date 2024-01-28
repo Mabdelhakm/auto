33183e5006340b1b4b5abac1820725af50842a16
loll <notEmail@tt.com>
2024-01-28 19:50:12 +0200

    dh

--- src/main/java/main/Setup.java
+++ src/main/java/main/Setup.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
